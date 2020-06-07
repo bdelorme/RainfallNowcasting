b1a3cc5385591e8bfb7c4a0a2819c5a36763c5c6
vincent-dufourdecieux <vincent.dufourdecieux@gmail.com>
2020-06-07 01:03:23 +0000

    Mini erreur dans run_all_convdlrm

--- Radar/run_all_convdlrm.py
+++ Radar/run_all_convdlrm.py
@@ -2,7 +2,8 @@ from pathlib import Path
 from subprocess import run
 
 
-for param_file in Path('experiments').glob('convdlrm*.json'):
+#for param_file in Path('experiments').glob('convdlrm*.json'):
+for param_file in Path('experiments').glob('convdlrm_lsm.json'):
     print(f'Run {param_file.stem}')
     run(['python', 'run_one_model.py', str(param_file)])
-    run(['python', 'apply_model_to_new_test.py', '--nargs', str(param_file), 13, 2018, 3])
+    run(['python', 'apply_model_to_new_test.py', '--nargs', str(param_file), '13', '2018', '3'])
