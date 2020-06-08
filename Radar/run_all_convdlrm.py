fe62920c7d7813d5707d3d1794af9b54482fd935
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-06-08 02:48:12 +0000

    modified nk convdlrm tests

--- Radar/run_all_convdlrm.py
+++ Radar/run_all_convdlrm.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from subprocess import run
 
 
-for param_file in Path('experiments').glob('convdlrm*.json'):
+for param_file in Path('experiments').glob('convdlrm_dew*.json'):
     print(f'Run {param_file.stem}')
     run(['python', 'run_one_model.py', str(param_file)])
     run(['python', 'apply_model_to_test.py', '--nargs', str(param_file), '10', '2018', '3'])
